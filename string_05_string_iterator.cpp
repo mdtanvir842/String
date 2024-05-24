af8a9267e16076cb9bfeacae6d7e0f5d004951c6
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-24 10:45:55 +0600

    Create string_05_string_iterator.cpp

---
+++ string_05_string_iterator.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="Md Tanvir Alam";
+    string::iterator it;
+    for(it=s.begin();it<s.end();it++){
+        cout<<*it;
+    }
+}
