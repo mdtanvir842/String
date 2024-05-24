e62631fae60210bc01b5e0c06439747329d2835a
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-24 10:40:01 +0600

    Create string_04_string-concatenation.cpp

---
+++ string_04_string-concatenation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="Md";
+    string s1="Tanvir";
+    string temp=s+" "+s1;
+    cout<<temp<<endl;
+    temp=temp+' '+"Alam";
+    cout<<temp<<endl;
+    s=s+" "+s1;
+    cout<<s<<endl;
+}
