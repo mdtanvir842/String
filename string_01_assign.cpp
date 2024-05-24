6e84a59a8a3daa4f0e50165412d50c4607d6bb77
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-05-24 10:26:21 +0600

    Create string_01_assign.cpp

---
+++ string_01_assign.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="abcde";
+    string st;
+    s+='f';
+    s+='g';
+    cout<<s<<endl;
+    cout<<s.size()<<endl;
+    cin>>st;
+    cout<<st<<endl;
+}
