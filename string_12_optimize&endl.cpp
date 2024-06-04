46b2f42079eaeb3be112055d0d1807de27bd5b71
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-04 20:52:34 +0600

    Create string_12_optimize&endl.cpp

---
+++ string_12_optimize&endl.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string x,y,z;
+    cin>>x;
+    cin>>y;
+    cin>>z;
+    string p=x+y+z;
+    cout<<p<<endl;
+}
