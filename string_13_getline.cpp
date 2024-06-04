f8277d481ea526bda9bca0a4e47133043a5b9017
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-04 20:59:20 +0600

    Create string_13_getline.cpp

---
+++ string_13_getline.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string x;
+    getline(cin,x);
+    cout<<x<<endl;
+}
