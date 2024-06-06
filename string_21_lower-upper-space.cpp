c18c7c02b499e71f773afc49ac96931c0ffc516c
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-06 16:11:31 +0600

    Create string_21_lower-upper-space.cpp

---
+++ string_21_lower-upper-space.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    char s='a';
+    bool a=isupper(s);//int also show correct result
+    cout<<a<<endl;
+    a=islower(s);
+    cout<<a<<endl;
+
+    a=isupper(s);
+    cout<<a<<endl;
+    a=islower(s);
+    cout<<a<<endl;
+    a=isspace(s);
+    cout<<a<<endl;
+    s=' ';
+    a=isspace(s);
+    cout<<a<<endl;
+    s='a';
+    s=toupper(s);
+    cout<<s<<endl;
+    s='B';
+    s=tolower(s);
+    cout<<s<<endl;
+
+}
