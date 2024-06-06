dc752bbd2e515e05f3e8b1eb992accbc056849c1
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-06 15:33:32 +0600

    Create string_20_erase.cpp

---
+++ string_20_erase.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string s="abcckakajafaj";
+    s.erase(s.begin()+4,s.begin()+8);
+    cout<<s<<endl;
+}
