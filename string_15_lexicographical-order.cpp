e3710e7edcf18143a83e4401d1e71a1a2eed7700
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-06 14:06:53 +0600

    Create string_15_lexicographical-order.cpp

---
+++ string_15_lexicographical-order.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    vector<string>v;
+    v.push_back("My");
+    v.push_back("name");
+    v.push_back("is");
+    v.push_back("Md");
+    v.push_back("ab");
+    v.push_back("aa");
+    v.push_back("aaa");
+
+    v.push_back("Tanvir");
+    v.push_back("Alam");
+    sort(v.begin(),v.end());
+    for(auto i:v){
+        cout<<i<<endl;
+    }
+}
