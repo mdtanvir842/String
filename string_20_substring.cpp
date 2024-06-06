786d138df0572a5b81854dba1476fc8b65714da2
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-06 15:45:10 +0600

    Create string_20_substring.cpp

---
+++ string_20_substring.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string s="abcckakajafaj";
+    if(s.find("abc")!=-1){
+        cout<<"Substring"<<endl;
+    }
+    else{
+        cout<<"Not Substring"<<endl;
+    }
+}
