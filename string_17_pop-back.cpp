65d877f5d609c1edef25a1b42159194c96b7036b
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-06 14:13:38 +0600

    Create string_17_pop-back.cpp

---
+++ string_17_pop-back.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string s="Abcdge";
+    cout<<s.back()<<endl;
+    s.pop_back();
+    cout<<s.back()<<endl;
+}
