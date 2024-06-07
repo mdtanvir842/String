10a562af876404d2ee24e95a7740c343c3ce3e98
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-07 16:27:04 +0600

    Create string_24_compare-string-stl.cpp

---
+++ string_24_compare-string-stl.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string s1="aaaaa";
+    string s2="aaaa";
+    cout<<s1.compare(s2)<<endl;
+    //smaller -1
+    //same 0
+    //greater 1
+}
