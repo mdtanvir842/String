5d0eb2b116ecf01d7695c47158f916a2ee99583c
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-07 17:45:46 +0600

    Create string_25_count.cpp

---
+++ string_25_count.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);
+#define endl '\n'
+int main(){
+    optimize();
+    string s="anabj";
+    int a=count(s.begin(),s.end(),'a');
+    cout<<a<<endl;
+}
