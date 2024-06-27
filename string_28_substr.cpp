7bad91056c885a25cd1a2f148de01f592d33553b
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-06-27 22:49:16 +0600

    Create string_28_substr.cpp

---
+++ string_28_substr.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1="abcde";
+
+    cout<<s1.substr(2,2)<<endl;//substr(position,total size want to remove for replacement)
+}
